5769f8b33941828836a2a22c303c28938a695c71
Steve-design <smzalendo31@gmail.com>
2019-11-28 17:37:35 +0300

    add a view function for the home projects

--- jiraniapp/views.py
+++ jiraniapp/views.py
@@ -10,3 +10,44 @@ from .email import send_welcome_email
 from django.urls import reverse
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home_projects (request):
+    # Display all projects here:
+
+    if request.GET.get('search_term'):
+        businesses = Business.search_businesses(request.GET.get('search_term'))
+
+    else:
+        businesses = Business.objects.all()
+
+    if request.GET.get('search_term'):
+        neighbourhoods = Neighbourhood.search_neighbourhood(request.GET.get('search_term'))
+
+    else:
+        neighbourhoods = Neighbourhood.objects.all()
+
+
+    if request.GET.get('search_term'):
+        projects = Project.search_project(request.GET.get('search_term'))
+
+    else:
+        projects = Project.objects.all()
+
+    form = NewsLetterForm
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST or None)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name=name, email=email)
+            recipient.save()
+            send_welcome_email(name, email)
+
+            HttpResponseRedirect('home_projects')
+
+
+    return render(request, 'index.html', {'projects':projects, 'letterForm':form,
+                                          'businesses':businesses,
+                                          'neighbourhoods':neighbourhoods})
