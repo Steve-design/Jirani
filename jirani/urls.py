446dc92d9c3a29c3d337f85f1f4a9699e9b42925
Steve-design <smzalendo31@gmail.com>
2019-11-28 15:10:40 +0300

    add url patterns

--- jirani/urls.py
+++ jirani/urls.py
@@ -18,4 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('jiraniapp.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^api-token-auth/', obtain_auth_token),
 ]
