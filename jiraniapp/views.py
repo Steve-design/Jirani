9a9f82893f54917926978bc5ac6fb4235559dded
Steve-design <smzalendo31@gmail.com>
2019-11-28 17:35:32 +0300

    import required modules

--- jiraniapp/views.py
+++ jiraniapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from __future__ import unicode_literals
+from .models import *
+from django.http  import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import *
+from django.contrib import messages
+from .email import send_welcome_email
+from django.urls import reverse
 
 # Create your views here.
