dd430e48c90daae4146b72d2aaf458ad36999cb8
Steve-design <smzalendo31@gmail.com>
2019-11-28 16:14:34 +0300

    add a class model for tags

--- jiraniapp/models.py
+++ jiraniapp/models.py
@@ -6,3 +6,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.sql.datastructures import Join
 
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tags(self):
+        self.save()
+
+    def delete_tags(self):
+        self.delete()
