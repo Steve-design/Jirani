e577c2b3e7e69f84d96b0523016167240d62d59d
Steve-design <smzalendo31@gmail.com>
2019-11-28 15:11:37 +0300

    import views and auth token

--- jirani/urls.py
+++ jirani/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
 from django.conf.urls import url
 from django.contrib import admin
 
