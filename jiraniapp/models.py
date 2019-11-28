c19fd84c28e73a5b9e6c948aa2d3e9c2f8ebe902
Steve-design <smzalendo31@gmail.com>
2019-11-28 16:15:42 +0300

    add a class model for location

--- jiraniapp/models.py
+++ jiraniapp/models.py
@@ -17,3 +17,15 @@ class tags(models.Model):
 
     def delete_tags(self):
         self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name        
