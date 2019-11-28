ff0b4fc528cafabdf6eac7109b895e6235be45cf
Steve-design <smzalendo31@gmail.com>
2019-11-28 16:13:28 +0300

    import required modules

--- jiraniapp/models.py
+++ jiraniapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from __future__ import unicode_literals
+import numpy as np
+from django.db.models.signals import post_save
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.sql.datastructures import Join
 
-# Create your models here.
