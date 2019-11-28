c1ad7ed16d9df0736633b1a1ccd78f30c11b7ae3
Steve-design <smzalendo31@gmail.com>
2019-11-28 17:30:22 +0300

    add required modules

--- jiraniapp/tests.py
+++ jiraniapp/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from __future__ import unicode_literals
+from .models import *
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 
 # Create your tests here.
